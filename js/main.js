f190819c8b6c86356731fbc38d66b3189d3f0876
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-09-30 14:39:03 +0400

    по классу .owl-carousel будет найдена обвёртка и внутри  запуститься карусель

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//вызвала карусель с с owl.carousel.js
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(); //по классу .owl-carousel будет найдена обвёртка и внутри  запуститься карусель
+  });
