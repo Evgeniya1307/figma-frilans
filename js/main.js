e1f621953f0d424cb149a06506c50bfe141aeb68
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-09-30 15:57:46 +0400

    <!--стрелки вправо влево кнопки переключения добавила-->

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 //вызвала карусель с с owl.carousel.js
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      center:true, //доб-ет класс центр к центрерованному эл-ту
-    loop:true, //чтобы циклить прокрутка
-    margin:30,//между слайдами чтобы был отсуп 
-  startPosition:1, //стартовал с 1 индекса
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    center: true, //доб-ет класс центр к центрерованному эл-ту
+    loop: true, //чтобы циклить прокрутка
+    margin: 30, //между слайдами чтобы был отсуп
+    startPosition: 1, //стартовал с 1 индекса
+    items: 3, //слайда за раз
   }); //по классу .owl-carousel будет найдена обвёртка и внутри  запуститься карусель
-  });
+});
